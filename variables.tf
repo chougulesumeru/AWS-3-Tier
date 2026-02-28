b6831201c98c70839a3ad7aca7888aa4bee19588
chougulesumeru <chougulesumeru19@gmail.com>
2026-02-28 21:49:30 +0530

    variables.tf modified

--- variables.tf
+++ variables.tf
@@ -1,16 +1,28 @@
 variable "aws_region" {
-   default = "us-east-2"
-   type= string
-   description = "aws-region for instance"
+  default     = "us-east-2"
+  type        = string
+  description = "aws-region for instance"
 }
 
 variable "instance_type" {
-    default = "t2.medium"
-    type="string"
+  default = "t2.medium"
+  type    = string
 }
 
 variable "key_name" {
-    default = null
-    type= string
-    description = "existing ec2 key-pair"
+  default     = null
+  type        = string
+  description = "existing ec2 key-pair"
+}
+
+variable "project_name" {
+  default     = "webshop"
+  type        = string
+  description = "project-short-name"
+}
+
+variable "environment" {
+  default     = "dev"
+  type        = string
+  description = "dev environment for ec2 instance"
 }
